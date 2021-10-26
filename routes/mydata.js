f0e3f12390acf7dd567a202facbb235e7af1a97b
Ajay Kumar Vemula <89420749+ajaykumarvemula@users.noreply.github.com>
2021-10-26 00:14:54 -0500

    kk

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ajay Kumar Vemula' });
+  res.render("mydata", { 
+      title: 'Ajay Kumar Vemula',
+      fact: "I love myself"
+     });
 });
 
 module.exports = router;
